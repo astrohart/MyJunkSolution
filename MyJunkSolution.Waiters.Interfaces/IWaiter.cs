5ee910e6fe0c785c528c6f2c8dbdb39871cb65a2
Brian Hart <brian.hart@xylogix.net>
2021-03-21 15:16:40 -0400

    Add overload and fluent builder method
    Added an overload of WaitForUserToPressAKey that allows the developer to specify a user prompt; and an AttachDisplayer fluent builder method that allows us to associate the Waiter with a Displayer.

--- MyJunkSolution.Waiters.Interfaces/IWaiter.cs
+++ MyJunkSolution.Waiters.Interfaces/IWaiter.cs
@@ -1,4 +1,6 @@
-﻿namespace MyJunkSolution.Waiters.Interfaces
+﻿using MyJunkSolution.Displayers.Interfaces;
+
+namespace MyJunkSolution.Waiters.Interfaces
 {
     /// <summary>
     /// Defines the publicly-exposed methods and properties of a Waiter object.
@@ -9,8 +11,36 @@
     public interface IWaiter
     {
         /// <summary>
+        /// Associates this
+        /// <see
+        ///     cref="T:MyJunkSolution.Waiters.Interfaces.IWaiter" />
+        /// with a
+        /// <see
+        ///     cref="T:MyJunkSolution.Displayers.Interfaces.IDisplayer" />
+        /// to be
+        /// used for displaying text to the user.
+        /// </summary>
+        /// <returns>
+        /// Reference to the same instance of the object that called this
+        /// method, for fluent use.
+        /// </returns>
+        IWaiter AttachDisplayer(IDisplayer displayer);
+
+        /// <summary>
         /// Delays the application until the user presses a key.
         /// </summary>
         void WaitForUserToPressAKey();
+
+        /// <summary>
+        /// Delays the application until the user presses a key.
+        /// </summary>
+        /// <param name="prompt">
+        /// (Required.) String containing the prompt to display for the user.
+        /// </param>
+        /// <exception cref="T:System.ArgumentException">
+        /// Thrown if the required parameter, <paramref name="prompt" />, is
+        /// passed a blank or <see langword="null" /> string for a value.
+        /// </exception>
+        void WaitForUserToPressAKey(string prompt);
     }
 }
