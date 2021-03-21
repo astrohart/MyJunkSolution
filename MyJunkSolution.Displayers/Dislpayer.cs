c232a61614c301b4f05fe6aea79663eb918b3181
Brian Hart <brian.hart@xylogix.net>
2021-03-21 14:43:50 -0400

    Create duisplayer class

---
+++ MyJunkSolution.Displayers/Dislpayer.cs
@@ -0,0 +1,12 @@
+﻿using MyJunkSolution.Interfaces;
+using System;
+using System.Runtime.InteropServices;
+
+namespace MyJunkSolution.Displayers
+{
+    public class Dislpayer : IDisplayer
+    {
+        public void Dislpay()
+            => Console.WriteLine("Have a wonderful Disney day!");
+    }
+}
