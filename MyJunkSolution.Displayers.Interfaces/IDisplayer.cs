8402ed28372a035ce07d48f0851ea1452f6b09a7
Brian Hart <brian.hart@xylogix.net>
2021-03-21 14:56:17 -0400

    Add XML docs

--- MyJunkSolution.Displayers.Interfaces/IDisplayer.cs
+++ MyJunkSolution.Displayers.Interfaces/IDisplayer.cs
@@ -1,13 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace MyJunkSolution.Displayers.Interfaces
 {
+    /// <summary>
+    /// Defines the publicly-exposed methods and properties of a Displayer object.
+    /// </summary>
+    /// <remarks>
+    /// Displayer objects print text on the screen.
+    /// </remarks>
     public interface IDisplayer
     {
+        /// <summary>
+        /// Displays text for the user.
+        /// </summary>
         void Dislpay();
     }
 }
