0c66e8d28eb994b40d52e0a1655caf391d7662f7
Brian Hart <brian.hart@xylogix.net>
2021-03-21 15:08:08 -0400

    Create IWaiter interface and document it

---
+++ MyJunkSolution.Waiters.Interfaces/IWaiter.cs
@@ -0,0 +1,16 @@
+﻿namespace MyJunkSolution.Waiters.Interfaces
+{
+    /// <summary>
+    /// Defines the publicly-exposed methods and properties of a Waiter object.
+    /// </summary>
+    /// <remarks>
+    /// Waiters wait for something interesting to happen.
+    /// </remarks>
+    public interface IWaiter
+    {
+        /// <summary>
+        /// Delays the application until the user presses a key.
+        /// </summary>
+        void WaitForUserToPressAKey();
+    }
+}
