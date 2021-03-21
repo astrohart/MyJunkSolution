cd01102d6f67fc5032100fa9779c5b4d23bfbe0a
Brian Hart <brian.hart@xylogix.net>
2021-03-21 15:12:13 -0400

    Create 'MakeNewWaiter' factory class

---
+++ MyJunkSolution.Waiters.Factories/MakeNewWaiter.cs
@@ -0,0 +1,29 @@
+﻿using MyJunkSolution.Waiters.Interfaces;
+
+namespace MyJunkSolution.Waiters.Factories
+{
+    /// <summary>
+    /// Creates and initializes new instances of objects that implement the
+    /// <see
+    ///     cref="T:MyJunkSolution.Waiters.Interfaces.IWaiter" />
+    /// interface.
+    /// </summary>
+    public static class MakeNewWaiter
+    {
+        /// <summary>
+        /// Creates a new instance of an object that implements the
+        /// <see
+        ///     cref="T:MyJunkSolution.Waiters.Interfaces.IWaiter" />
+        /// interface and
+        /// returns a reference to it.
+        /// </summary>
+        /// <returns>
+        /// Reference to an instance of an object that implements the
+        /// <see
+        ///     cref="T:MyJunkSolution.Waiters.Interfaces.IWaiter" />
+        /// interface.
+        /// </returns>
+        public static IWaiter FromScratch()
+            => new Waiter();
+    }
+}
