9a1339214d8915f074f2b14ba0f5e49caef4f7e0
Brian Hart <brian.hart@xylogix.net>
2021-04-10 08:50:20 -0400

    Cleanup code

--- MyJunkSolution.Processors.Factories/MakeNewProcessor.cs
+++ MyJunkSolution.Processors.Factories/MakeNewProcessor.cs
@@ -1,30 +1,29 @@
 using MyJunkSolution.Processors.Interfaces;
-using MyJunkSolution.Processors;
 
 namespace MyJunkSolution.Processors.Factories
 {
     /// <summary>
     /// Creates and initializes new instances of objects that implement the
     /// <see
-    ///     cref="T:MyJunkSolution.Processors.Interfaces.IProcessor" />
+    ///   cref="T:MyJunkSolution.Processors.Interfaces.IProcessor" />
     /// interface.
     /// </summary>
     public static class MakeNewProcessor
-    {
-        /// <summary>
-        /// Creates a new instance of an object that implements the
-        /// <see
-        ///     cref="T:MyJunkSolution.Processors.Interfaces.IProcessor" />
-        /// interface
-        /// and returns a reference to it.
-        /// </summary>
-        /// <returns>
-        /// Reference to an instance of an object that implements the
-        /// <see
-        ///     cref="T:MyJunkSolution.Processors.Interfaces.IProcessor" />
-        /// interface.
-        /// </returns>
-        public static IProcessor FromScratch()
-            => new Processor();
-    }
+  {
+      /// <summary>
+      /// Creates a new instance of an object that implements the
+      /// <see
+      ///   cref="T:MyJunkSolution.Processors.Interfaces.IProcessor" />
+      /// interface
+      /// and returns a reference to it.
+      /// </summary>
+      /// <returns>
+      /// Reference to an instance of an object that implements the
+      /// <see
+      ///   cref="T:MyJunkSolution.Processors.Interfaces.IProcessor" />
+      /// interface.
+      /// </returns>
+      public static IProcessor FromScratch()
+      => new Processor();
+  }
 }
